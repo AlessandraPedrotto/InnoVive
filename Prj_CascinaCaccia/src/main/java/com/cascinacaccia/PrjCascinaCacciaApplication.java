67ed92dff9ed3f89de575112d8af17bd80d70199
AlessandraPedrotto <alessandra.pedrotto@edu.itspiemonte.it>
2024-12-06 00:50:14 +0100

    Creazione del progetto in java

---
+++ Prj_CascinaCaccia/src/main/java/com/cascinacaccia/PrjCascinaCacciaApplication.java
@@ -0,0 +1,13 @@
+package com.cascinacaccia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjCascinaCacciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjCascinaCacciaApplication.class, args);
+	}
+
+}
---
+++ Prj_CascinaCaccia/src/test/java/com/cascinacaccia/PrjCascinaCacciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cascinacaccia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjCascinaCacciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
