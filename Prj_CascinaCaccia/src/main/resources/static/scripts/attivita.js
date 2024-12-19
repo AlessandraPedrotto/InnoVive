9809829b12d59bd9d27c324fda614b87eaa27bfc
Daniele Margiacchi <danielemargiacchi@gmail.com>
2024-12-19 15:59:50 +0100

    pagina attivita added, attivita.js added to manage nav tab menu

---
+++ Prj_CascinaCaccia/src/main/resources/static/scripts/attivita.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll('.secondary a');
+const medie = document.getElementById('medie');
+const superiori = document.getElementById('superiori');
+const tutti = document.getElementById('tutti') 
+
+
+//  for each button, adding an event listener
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();   // manage default browser's behaviour 
+
+      //  hiding all sections
+      medie.classList.add('hidden');               // da gestire con array
+      superiori.classList.add('hidden');
+      tutti.classList.add('hidden');
+
+      //  removing torunaments class
+      medie.classList.remove('attivita');
+      superiori.classList.remove('attivita');               // idem
+      tutti.classList.remove('attivita');
+      
+      //  removing active class from all buttons
+      buttons.forEach(btn => btn.classList.remove('active'));
+  
+      // showing only the clicked section
+      if (button.textContent === 'Medie') {
+        medie.classList.remove('hidden');
+        medie.classList.add('attivita');
+      } else if (button.textContent === 'Superiori') {
+        superiori.classList.remove('hidden');
+        superiori.classList.add('attivita');
+      } else if (button.textContent === 'Tutti') {
+        tutti.classList.remove('hidden');
+        tutti.classList.add('attivita');
+      }
+      //  adding active class to the selected section
+      button.classList.add('active');
+    });
+  });
