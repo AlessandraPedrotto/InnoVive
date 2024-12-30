5710a2fabcd06909e6fa9563167171e6a3b5f4f0
Daniele Margiacchi <danielemargiacchi@gmail.com>
2024-12-30 20:47:14 +0100

    attivita page html and script file

--- Prj_CascinaCaccia/src/main/resources/static/scripts/attivita.js
+++ Prj_CascinaCaccia/src/main/resources/static/scripts/attivita.js
@@ -1,23 +1,26 @@
 const buttons = document.querySelectorAll('.secondary a');
 const medie = document.getElementById('medie');
 const superiori = document.getElementById('superiori');
-const tutti = document.getElementById('tutti') ;
+const tutti = document.getElementById('tutti') 
+const laboratori = document.getElementById('laboratori') 
 
 
 //  for each button, adding an event listener
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-      e.preventDefault();   // manage default browser's behaviour 
+      e.preventDefault();
 
       //  hiding all sections
       medie.classList.add('hidden');               // da gestire con array
       superiori.classList.add('hidden');
       tutti.classList.add('hidden');
+      laboratori.classList.add('hidden');
 
       //  removing torunaments class
       medie.classList.remove('attivita');
       superiori.classList.remove('attivita');               // idem
       tutti.classList.remove('attivita');
+      laboratori.classList.remove('attivita');
       
       //  removing active class from all buttons
       buttons.forEach(btn => btn.classList.remove('active'));
@@ -29,6 +32,9 @@ buttons.forEach(button => {
       } else if (button.textContent === 'Superiori') {
         superiori.classList.remove('hidden');
         superiori.classList.add('attivita');
+      } else if (button.textContent === 'Laboratori') {
+        laboratori.classList.remove('hidden');
+        laboratori.classList.add('attivita');
       } else if (button.textContent === 'Tutti') {
         tutti.classList.remove('hidden');
         tutti.classList.add('attivita');
@@ -37,3 +43,24 @@ buttons.forEach(button => {
       button.classList.add('active');
     });
   });
+
+
+// accordion menu
+
+document.querySelectorAll('.accordion-header').forEach(header => {
+  header.addEventListener('click', () => {
+    const currentlyActive = document.querySelector('.accordion-header.active');
+    if (currentlyActive && currentlyActive !== header) {
+      currentlyActive.classList.remove('active');
+      currentlyActive.nextElementSibling.style.display = 'none';
+    }
+
+    header.classList.toggle('active');
+    const content = header.nextElementSibling;
+    if (header.classList.contains('active')) {
+      content.style.display = 'block';
+    } else {
+      content.style.display = 'none';
+    }
+  });
+});
