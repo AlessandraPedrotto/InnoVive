e8b6d41851874835966edbedb00a87e1fe935617
Daniele Margiacchi <danielemargiacchi@gmail.com>
2025-01-16 21:47:35 +0100

    removed laboratori section

--- Prj_CascinaCaccia/src/main/resources/static/scripts/attivita.js
+++ Prj_CascinaCaccia/src/main/resources/static/scripts/attivita.js
@@ -1,8 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
 const buttons = document.querySelectorAll('.secondary a');
 const medie = document.getElementById('medie');
 const superiori = document.getElementById('superiori');
 const tutti = document.getElementById('tutti') 
-const laboratori = document.getElementById('laboratori') 
 
 
 //  for each button, adding an event listener
@@ -18,13 +18,11 @@ buttons.forEach(button => {
       medie.classList.add('hidden');               // da gestire con array
       superiori.classList.add('hidden');
       tutti.classList.add('hidden');
-      laboratori.classList.add('hidden');
 
       //  removing torunaments class
       medie.classList.remove('attivita');
       superiori.classList.remove('attivita');               // idem
       tutti.classList.remove('attivita');
-      laboratori.classList.remove('attivita');
       
       //  removing active class from all buttons
       buttons.forEach(btn => btn.classList.remove('active'));
@@ -36,9 +34,6 @@ buttons.forEach(button => {
       } else if (button.textContent === 'Superiori') {
         superiori.classList.remove('hidden');
         superiori.classList.add('attivita');
-      } else if (button.textContent === 'Laboratori') {
-        laboratori.classList.remove('hidden');
-        laboratori.classList.add('attivita');
       } else if (button.textContent === 'Tutti') {
         tutti.classList.remove('hidden');
         tutti.classList.add('attivita');
@@ -46,25 +41,5 @@ buttons.forEach(button => {
       //  adding active class to the selected section
       button.classList.add('active');
     });
-  });
-
-
-// accordion menu
-
-document.querySelectorAll('.accordion-header').forEach(header => {
-  header.addEventListener('click', () => {
-    const currentlyActive = document.querySelector('.accordion-header.active');
-    if (currentlyActive && currentlyActive !== header) {
-      currentlyActive.classList.remove('active');
-      currentlyActive.nextElementSibling.style.display = 'none';
-    }
-
-    header.classList.toggle('active');
-    const content = header.nextElementSibling;
-    if (header.classList.contains('active')) {
-      content.style.display = 'block';
-    } else {
-      content.style.display = 'none';
-    }
   });
 });
