066dfce2c8a3fc35f09761cba4584139b16cb87a
AlessandraPedrotto <alessandra.pedrotto@edu.itspiemonte.it>
2025-01-21 10:55:25 +0100

    Commenti config

--- Prj_CascinaCaccia/src/main/java/com/cascinacaccia/config/CustomSessionExpiredStrategy.java
+++ Prj_CascinaCaccia/src/main/java/com/cascinacaccia/config/CustomSessionExpiredStrategy.java
@@ -9,8 +9,16 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/*
+ * CustomSessionExpiredStrategy implements SessionInformationExpiredStrategy 
+ * to define the behavior when a user's session expires.
+ * 
+ * This implementation listens for a session expiration event and redirects the user 
+ * to the login page with a custom error message indicating that the session has expired.
+ */
 public class CustomSessionExpiredStrategy implements SessionInformationExpiredStrategy {
-    @Override
+    
+	@Override
     public void onExpiredSessionDetected(SessionInformationExpiredEvent event) 
             throws IOException, ServletException {
         HttpServletRequest request = event.getRequest();
@@ -19,5 +27,4 @@ public class CustomSessionExpiredStrategy implements SessionInformationExpiredSt
         // Redirect to /login with a custom error message
         response.sendRedirect("/login?errorSession=sessionExpired");
     }
-
 }
